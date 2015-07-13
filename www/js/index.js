800706d6b5f52d0542c2718c0eb1a8ccd7e7da56
root <root@ubuntu.(none)>
2015-07-13 02:39:42 +0200

    notification sound test 	modified:   www/js/index.js

--- www/js/index.js
+++ www/js/index.js
@@ -49,6 +49,7 @@ var app = {
 											message:    'Evo Notifikacije !!!',  // The message that is displayed
 											title:      'Naslov',  // The title of the message
 											repeat:     'minutely',  // Either 'secondly', 'minutely', 'hourly', 'daily', 'weekly', 'monthly' or 'yearly'
+											sound: 		"file://mnt/Zagrli.mp3",
 											badge:      12  // Displays number badge to notification
 										});
 
